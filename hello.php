c8106ad745e552b5f309af262aa89176f21174c2
Kodai Okazaki <gboc374@gmail.com>
2015-10-30 10:05:31 +0900

    追加

--- hello.php
+++ hello.php
@@ -1,6 +1,20 @@
 <?php
 
 $hello = "hello wolrd!";
+function is small($a, $b){
+if($a < $b){
+return true;
+}else{
+return false;
+}
+}
 
-echo hello;
+$val1 = 20;
+$val2 = 30;
 
+if(is_small($val1, $val2)){
+echo "\$val1は\$val2より小さい\n";
+}else{
+echo "\$val2は\$val2と等しいか大きい\n";
+}
+?>
